fbe8e07788f808c4cd83f736ba25e210d6e1f500
Sahil Dinanath <sahildinanath@debian.local>
2023-10-04 12:52:20 +0200

    added colour scheme and added linting support

--- lua/custom/plugins/color_theme.lua
+++ lua/custom/plugins/color_theme.lua
@@ -1,9 +1,18 @@
 return {
 	-- theme
-	'catppuccin/nvim',
-	name = 'catppuccin',
-	priority = 5000,
-	config = function()
-		vim.cmd.colorscheme 'catppuccin-mocha'
-	end,
+	{
+		'catppuccin/nvim',
+		name = 'catppuccin',
+		priority = 5000,
+		config = function()
+			vim.cmd.colorscheme 'catppuccin-mocha'
+		end,
+	}
+	-- {
+	-- 	'RRethy/nvim-base16',
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.cmd.colorscheme 'base16-horizon-dark'
+	-- 	end
+	-- }
 }
